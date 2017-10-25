83098e939d3caeb0b682af80c78f6627e51be03b
Jonas Berlin <jonas.berlin@nitorcreations.com>
2017-10-25 18:58:29 +0200

    Typescript typings

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+function unquotedValidator(propertyName: string): {
+  needsQuotes: boolean,
+  needsBrackets: boolean,
+  es3Warning: boolean,
+  quotedValue: string,
+};
+
+export = unquotedValidator;
